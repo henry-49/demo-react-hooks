2c2991d7c1b9fe97f0e709ca9f2e252a05f0bdb7
henry-49 <henryswebdevelopment@gmail.com>
2021-08-11 15:23:53 +0200

    UseContext  Demo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import UseContextTutorial from './UseContextDemo/UseContextTutorial';
 import StateDemo from './UseState/StateDemo';
 
 function App() {
   return (
     <div className="App">
       <StateDemo></StateDemo>
+      <UseContextTutorial />
     </div>
   );
 }
---
+++ src/UseContextDemo/UseContextTutorial.js
@@ -0,0 +1,20 @@
+import React, { useState, createContext } from 'react';
+import Login from './Login';
+import User from './User';
+
+export const AppContext = createContext(null);
+
+
+ function UseContextTutorial() {
+     const [username, setUsername] = useState("");
+
+    return (
+        <div>
+            <AppContext.Provider value={{ username, setUsername }}>
+             <Login /> <User />
+            </AppContext.Provider>
+        </div>
+    )
+}
+
+export default UseContextTutorial;
